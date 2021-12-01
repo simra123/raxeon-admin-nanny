e3475ce1eaee30bf6f03eb3c83c63c9b33e9adae
Simra <72741150+simra123@users.noreply.github.com>
2021-12-01 21:11:37 +0500

    Updates

--- static/js/192.4981652b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[192],{1885:function(a,s){}}]);
-//# sourceMappingURL=192.4981652b.chunk.js.map
